bab291dfbd0c46d4d37f68353c41866c56da807c
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2020-12-30 10:58:19 -0700

    Update latest Polkadot Apps

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -14,8 +14,6 @@ import { createRococo } from './testingRococo';
 
 export function createTesting (t: TFunction): LinkOption[] {
   return [
-    // polkadot test relays
-    createRococo(t),
     // alphabetical based on chain name
     {
       info: 'centrifuge',
@@ -24,140 +22,16 @@ export function createTesting (t: TFunction): LinkOption[] {
       value: 'wss://fullnode.amber.centrifuge.io'
     },
     {
-      info: 'nodle',
-      text: t('rpc.nodle-arcadia', 'Arcadia', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Nodle' } }),
-      value: 'wss://arcadia1.nodleprotocol.io'
+      info: 'centrifuge',
+      text: t('rpc.flint', 'Flint', { ns: 'apps-config' }),
+      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Centrifuge' } }),
+      value: 'wss://fullnode.flint.centrifuge.io'
     },
     {
-      info: 'edgeware',
-      text: t('rpc.beresheet', 'Beresheet', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Commonwealth Labs' } }),
-      value: 'wss://beresheet1.edgewa.re'
+      info: 'centrifuge',
+      text: t('rpc.fulvous', 'Fulvous', { ns: 'apps-config' }),
+      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Centrifuge' } }),
+      value: 'wss://fullnode.fulvous.centrifuge.io'
     },
-    {
-      info: 'bifrost',
-      text: t('rpc.bifrost', 'Bifrost Asgard', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Bifrost' } }),
-      value: 'wss://testnet.liebi.com'
-    },
-    {
-      info: 'canvas',
-      text: t('rpc.canvas', 'Canvas', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Parity' } }),
-      value: 'wss://canvas-rpc.parity.io'
-    },
-    {
-      info: 'crust',
-      text: t('rpc.crust.network', 'Crust Maxwell CC2', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Crust Network' } }),
-      value: 'wss://api.crust.network/'
-    },
-    {
-      info: 'datahighway',
-      isDisabled: true,
-      text: t('rpc.datahighway.harbour', 'Harbour', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'MXC' } }),
-      value: 'wss://testnet-harbour.datahighway.com'
-    },
-    {
-      info: 'dock-testnet',
-      text: t('rpc.dock-testnet', 'Dock Testnet', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Dock Association' } }),
-      value: 'wss://danforth-1.dock.io'
-    },
-    {
-      info: 'dusty',
-      text: t('rpc.dusty', 'Dusty', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Stake Technologies' } }),
-      value: 'wss://rpc.dusty.plasmnet.io/'
-    },
-    {
-      info: 'encointer_cantillon',
-      text: t('rpc.encointer.cantillon', 'Encointer Cantillon', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Encointer Association' } }),
-      value: 'wss://cantillon.encointer.org'
-    },
-    {
-      info: 'encointer_gesell',
-      text: t('rpc.encointer.gesell', 'Encointer Gesell', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Encointer Association' } }),
-      value: 'wss://gesell.encointer.org'
-    },
-    {
-      info: 'equilibrium',
-      text: t('rpc.equilibriumtestnet', 'Equilibrium Testnet', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Equilibrium' } }),
-      value: 'wss://api.mvp.testnet.equilibrium.io'
-    },
-    {
-      info: 'substrate',
-      text: t('rpc.flamingfir', 'Flaming Fir', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Parity' } }),
-      value: 'wss://substrate-rpc.parity.io'
-    },
-    {
-      dnslink: 'galois',
-      info: 'galois',
-      text: t('rpc.galois', 'Galois', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'MathWallet' } }),
-      value: 'wss://galois.maiziqianbao.net/ws'
-    },
-    {
-      info: 'acala',
-      text: t('rpc.mandala', 'Mandala', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Acala' } }),
-      value: 'wss://node-6714447553211260928.rz.onfinality.io/ws'
-    },
-    {
-      info: 'kilt',
-      text: t('rpc.kilt', 'Mashnet', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'KILT Protocol' } }),
-      value: 'wss://full-nodes.kilt.io:9944/'
-    },
-    {
-      info: 'moonbaseAlpha',
-      text: t('rpc.moonbeam', 'Moonbase Alpha', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Moonbeam Network' } }),
-      value: 'wss://wss.testnet.moonbeam.network'
-    },
-    {
-      info: 'phala',
-      text: t('rpc.phala', 'Phala PoC-3', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Phala Network' } }),
-      value: 'wss://poc3a.phala.network/ws'
-    },
-    {
-      info: 'polkabtc',
-      text: t('rpc.polkabtc', 'PolkaBTC', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Interlay' } }),
-      value: 'wss://alpha.polkabtc.io/api/parachain'
-    },
-    {
-      info: 'laminar',
-      text: t('rpc.turbulence', 'Turbulence', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Laminar' } }),
-      value: 'wss://testnet-node-1.laminar-chain.laminar.one/ws'
-    },
-    {
-      info: 'sora-substrate',
-      text: t('rpc.sora-substrate', 'SORA-staging', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Soramitsu' } }),
-      value: 'wss://ws.stage.sora2.soramitsu.co.jp'
-    },
-    {
-      dnslink: 'westend',
-      info: 'westend',
-      text: t('rpc.westend', 'Westend', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Parity' } }),
-      value: 'wss://westend-rpc.polkadot.io'
-    },
-    {
-      dnslink: 'zero',
-      info: 'zero',
-      text: t('rpc.zero', 'zero', { ns: 'apps-config' }),
-      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'ZERO' } }),
-      value: 'wss://alphaville-0.zero.io'
-    }
   ];
 }
